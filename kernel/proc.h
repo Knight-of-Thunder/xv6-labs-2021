46cec8495218a7d0a18ef20f3938742f80e2c4a6
ding <1605383434@qq.com>
2025-03-01 15:51:18 +0800

    lab 2：System call tracing complete

--- kernel/proc.h
+++ kernel/proc.h
@@ -105,4 +105,7 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+
+  // lab2: add new variable in proc for sys_trace
+  int mask;                   
 };
