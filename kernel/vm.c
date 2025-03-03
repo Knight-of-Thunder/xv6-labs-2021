cff1f4adec75874ddc9ad260f22c1e1a8f4c520f
ding <1605383434@qq.com>
2025-03-03 16:50:09 +0800

    complete: Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,35 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+// recursively print page table for vmprint
+void pdprint(pagetable_t pagetable, int level)
+{
+  if(level < 0)
+    return;
+  for(int i = 0; i<512; i++){
+    int clevel = level;
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      if(level == 2)
+        printf("..");
+      else if(level == 1)
+        printf(".. ..");
+      else
+        printf(".. .. ..");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+      uint64 child = PTE2PA(pte);
+      pdprint((pagetable_t)child, --clevel);
+    }
+  }
+}
+
+// pirnt page table
+void vmprint(pagetable_t pagetable)
+{
+  int level = 2;
+  printf("page table %p\n", pagetable);
+  pdprint(pagetable, level);
+}
+
+
